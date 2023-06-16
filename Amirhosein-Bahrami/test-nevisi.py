a0c36e8e51f3fb61f3d22e1adaa188993b165378
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2023-06-17 00:14:24 +0330

    unit tests added

--- Amirhosein-Bahrami/test-nevisi.py
+++ Amirhosein-Bahrami/test-nevisi.py
@@ -1,5 +1,47 @@
+import unittest
+
 def add_numbers(a, b):
     if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
         raise ValueError("Inputs must be numeric.")
     sum = a + b
     return sum
+
+
+class AddNumbersTestCase(unittest.TestCase):
+    def test_add_numbers(self):
+        # Test case 1: Valid integer inputs
+        number1 = 5
+        number2 = 3
+        expected_result = 8
+        actual_result = add_numbers(number1, number2)
+        self.assertEqual(actual_result, expected_result)
+
+        # Test case 2: Valid float inputs
+        number3 = 2.5
+        number4 = 1.5
+        expected_result = 4.0
+        actual_result = add_numbers(number3, number4)
+        self.assertEqual(actual_result, expected_result)
+
+        # Test case 3: Negative inputs
+        number5 = -10
+        number6 = 5
+        expected_result = -5
+        actual_result = add_numbers(number5, number6)
+        self.assertEqual(actual_result, expected_result)
+
+        # Test case 4: Zero inputs
+        number7 = 0
+        number8 = 0
+        expected_result = 0
+        actual_result = add_numbers(number7, number8)
+        self.assertEqual(actual_result, expected_result)
+
+        # Test case 5: String inputs
+        string1 = "5"
+        string2 = "3"
+        with self.assertRaises(ValueError):
+            add_numbers(string1, string2)
+
+if __name__ == '__main__':
+    unittest.main()
