6b9943c902655744846b303f4ed75a509901c40c
unknown <zahragoleij0@gmail.com>
2023-06-30 22:30:57 +0330

    add unit test project

--- Zahra goleij/app.py
+++ Zahra goleij/app.py
@@ -1,17 +1,11 @@
 # project for Unit test
 
-# name :
-# student_number :
-# email :
+# name :ZahraGoleij
+# student_number :981608140
+# email :zahragoleij0@gmail.com
 
 def sum(a, b) :
     return a + b
 
-def minus(a, b) :
-    return a - b
-
 def multiple(a, b) :
     return a * b
-
-def division(a, b) :
-    return a / b
