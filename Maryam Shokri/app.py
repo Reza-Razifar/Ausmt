c0bbab72fe6cef0a8128064803aa694e2acfeb6b
MaryamShokri79 <123076687+MaryamShokri79@users.noreply.github.com>
2023-07-01 20:46:47 +0430

    adding unit test project

---
+++ Maryam Shokri/app.py
@@ -0,0 +1,12 @@
+# Project for Unit test
+
+# Name : Maryam Shokri
+# ID : 981608130
+# Email : maryam12shokri@gmail.com
+
+def sum(a, b) :
+    return a + b
+    
+
+def division(a, b) :
+    return a / b
