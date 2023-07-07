ca42f9abc4da0e90a903faef1c53914931e913ca
Armin <armingholami97gmail.com>
2023-07-07 14:25:26 +0330

    new function as sum added instead of is_prime with unittest

---
+++ Armin Gholami/main.py
@@ -0,0 +1,7 @@
+def sum_numbers():
+    number1 = int(input("عدد اول را وارد کنید: "))
+    number2 = int(input("عدد دوم را وارد کنید: "))
+    result = number1 + number2
+    print("مجموع دو عدد وارد شده: ", result)
+
+sum_numbers()
--- Armin Gholami/test.py
+++ Armin Gholami/test.py
@@ -1,14 +1,16 @@
 import unittest
-from prime import is_prime
 
-class TestPrime(unittest.TestCase):
+def sum_numbers(input1, input2):
+    return input1 + input2
 
-    def test_is_prime(self):
-        self.assertEqual(is_prime(2), True)
-        self.assertEqual(is_prime(3), True)
-        self.assertEqual(is_prime(7), True)
-        self.assertEqual(is_prime(10), False)
-        self.assertEqual(is_prime(21), False)
+class TestSumNumbers(unittest.TestCase):
+    
+    def test_sum_numbers(self):
+        self.assertEqual(sum_numbers(2, 3), 5)
+        self.assertEqual(sum_numbers(-2, 2), 0)
+        self.assertEqual(sum_numbers(0, 5), 5)
+        self.assertEqual(sum_numbers(-100, 100), 0)
+        self.assertEqual(sum_numbers(10, 10), 20)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
