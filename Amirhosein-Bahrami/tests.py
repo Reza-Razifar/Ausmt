9d9a4855e0d6fb8437ebd6e9219ccb08a84b5673
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2023-07-08 11:08:31 +0330

    modify:function and tests are now seperated

--- Amirhosein-Bahrami/tests.py
+++ Amirhosein-Bahrami/tests.py
@@ -1,10 +1,5 @@
+from . import def-sum
 import unittest
-
-def add_numbers(a, b):
-    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-        raise ValueError("Inputs must be numeric.")
-    sum = a + b
-    return sum
 
 
 class AddNumbersTestCase(unittest.TestCase):
