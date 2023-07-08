a28d69950ce6bb6305c1450e0b2029dd611346cb
Zahra <ashkani1653@gmail.com>
2023-07-08 19:50:44 +0430

    test file editted

--- Zahra ashkani/main_981608107.py
+++ Zahra ashkani/main_981608107.py
@@ -1,12 +1,2 @@
-
-def add_numbers(num1, num2):
-    result = num1 + num2
-    return result
-
-number1 = int(input("enter the first number"))
-number2 = int(input("enter the second number"))
-
-result = add_numbers(number1, number2)
-
-print("result= ", result)
-
+def add_numbers(a, b):
+    return a + b
--- Zahra ashkani/test_main_981608107.py
+++ Zahra ashkani/test_main_981608107.py
@@ -1,19 +1,4 @@
-import unittest
-from main_981608107 import add_numbers
-
-class TestAddNumbers(unittest.TestCase):
-
-    def test_add_positive_numbers(self):
-        result = add_numbers(3, 4)
-        self.assertEqual(result, 7)
-
-    def test_add_negative_numbers(self):
-        result = add_numbers(-5, -2)
-        self.assertEqual(result, -7)
-
-    def test_add_positive_negative_numbers(self):
-        result = add_numbers(8, -3)
-        self.assertEqual(result, 5)
-
-if __name__ == '__main__':
-    unittest.main()
+import pytest
+import main_981608107
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
