d5bf7149fbc6e7a6701b49741ce021d05ed61fc4
Zahra <ashkani1653@gmail.com>
2023-07-08 19:06:56 +0430

    some changes in main project and test project

--- Zahra ashkani/main_981608107.py
+++ Zahra ashkani/main_981608107.py
@@ -1,4 +1,12 @@
-def add_numbers(a, b):
-    return a + b
 
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+number1 = int(input("enter the first number"))
+number2 = int(input("enter the second number"))
+
+result = add_numbers(number1, number2)
+
+print("result= ", result)
 
--- Zahra ashkani/test_main_981608107.py
+++ Zahra ashkani/test_main_981608107.py
@@ -1,11 +1,19 @@
 import unittest
-import main_981608107.py
+from main_981608107 import add_numbers
 
 class TestAddNumbers(unittest.TestCase):
-    def test_add_numbers(self):
-        self.assertEqual(function.add_numbers(2, 3), 5)
-        self.assertEqual(function.add_numbers(-2, 3), 1)
-        self.assertEqual(function.add_numbers(0, 0), 0)
+
+    def test_add_positive_numbers(self):
+        result = add_numbers(3, 4)
+        self.assertEqual(result, 7)
+
+    def test_add_negative_numbers(self):
+        result = add_numbers(-5, -2)
+        self.assertEqual(result, -7)
+
+    def test_add_positive_negative_numbers(self):
+        result = add_numbers(8, -3)
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
