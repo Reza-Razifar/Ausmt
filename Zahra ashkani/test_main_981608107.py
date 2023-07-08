6e8a3faa74ac3dd0fc97f0f029a23c17087d108c
Zahra <ashkani1653@gmail.com>
2023-07-09 01:20:44 +0430

    some updates added

--- Zahra ashkani/test_main_981608107.py
+++ Zahra ashkani/test_main_981608107.py
@@ -1,6 +1,4 @@
-import main_981608107
+from main_981608107 import add_numbers
+
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
-    assert add_numbers(0, 0) == 0
-    assert add_numbers(-5, 5) == 0
-    assert add_numbers(10, -5) == 5
